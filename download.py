6e12b3a2476247dda4baef91e6e68da8d5fe8323
Djsurry <djsurry@gmail.com>
2018-12-25 15:23:35 -0500

    Update download.py

--- download.py
+++ download.py
@@ -51,9 +51,6 @@ def main():
     if not os.getenv("MUSIC"):
         print("Please export the envirnment varible music. Exiting...")
         exit()
-    if "downloaded-music" in os.listdir():
-        print("Please move or rename the file downloaded-music from the CWD. Exiting...")
-        exit()
 
     query = input("Search video or direct youtube link: ")
     if "https://" in query and 'youtube' in query and 'watch' in query:
